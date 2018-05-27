0552eb5765d248a414c15c0ccc0c0b21a25219d8
Jun Takeda <takeda.jun@gmail.com>
2018-05-27 20:01:39 +0900

    add check BLE

--- app/src/main/java/com/github/takjn/grrnble/MainActivity.java
+++ app/src/main/java/com/github/takjn/grrnble/MainActivity.java
@@ -1,13 +1,77 @@
 package com.github.takjn.grrnble;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_ENABLEBLUETOOTH = 1;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if system has BLE feature
+        if( !getPackageManager().hasSystemFeature( PackageManager.FEATURE_BLUETOOTH_LE ) ) {
+            Toast.makeText( this, R.string.ble_is_not_supported, Toast.LENGTH_SHORT ).show();
+            finish();
+            return;
+        }
+
+        // Get BluetoothAdapter
+        BluetoothManager bluetoothManager = (BluetoothManager)getSystemService( Context.BLUETOOTH_SERVICE );
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if( mBluetoothAdapter == null ) {
+            // System does not have Bluetooth service
+            Toast.makeText( this, R.string.bluetooth_is_not_supported, Toast.LENGTH_SHORT ).show();
+            finish();
+            return;
+        }
     }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+
+        super.onResume();
+        requestBluetoothFeature();
+    }
+
+    private void requestBluetoothFeature() {
+        if( mBluetoothAdapter.isEnabled() ) {
+            return;
+        }
+
+        Intent enableBtIntent = new Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE );
+        startActivityForResult( enableBtIntent, REQUEST_ENABLEBLUETOOTH );
+    }
+
+    @Override
+    protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
+        switch( requestCode ) {
+            case REQUEST_ENABLEBLUETOOTH:
+                if( Activity.RESULT_CANCELED == resultCode ) {
+                    Toast.makeText( this, R.string.bluetooth_is_not_working, Toast.LENGTH_SHORT ).show();
+                    finish();
+                    return;
+                }
+                break;
+        }
+        super.onActivityResult( requestCode, resultCode, data );
+    }
+
 }
