a24c1b099b56108792642760f067c3a79ad9d945
takjn <takeda.jun@gmail.com>
2018-07-04 00:53:06 +0900

    bug fix

--- GRRNble/GRRNble.ino
+++ GRRNble/GRRNble.ino
@@ -15,8 +15,8 @@ SSD1306AsciiWire oled;
 // settings for I/O pins
 #define BUZZER_PIN      10          // pin for buzzer
 #define KEY_PREV_PIN    3           // pin for previous button
-#define KEY_SELECT_PIN  2           // pin for select button
-#define KEY_NEXT_PIN    4           // pin for next button
+#define KEY_SELECT_PIN  4           // pin for select button
+#define KEY_NEXT_PIN    2           // pin for next button
 #define VOLTAGE_OUT_PIN A1          // pin for voltage measurement
 #define VOLTAGE_CHK_PIN A0          // pin for voltage measurement
 
